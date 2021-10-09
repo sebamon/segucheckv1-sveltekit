aea487bbd04130e0c437ed338fd0b7e9a95b7981
sebamon <sebamon12@gmail.com>
2021-10-09 18:28:23 -0300

    Creación del repositorio Segucheck.v1

---
+++ svelte.config.js
@@ -0,0 +1,29 @@
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [preprocess({
+        scss: {
+            "prependData": "@import \"src/variables.scss\";"
+        }
+    })],
+
+	kit: {
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: '#svelte',
+
+        vite: {
+            css: {
+                preprocessorOptions: {
+                    scss: {
+                        additionalData: "@import \"src/variables.scss\";"
+                    }
+                }
+            }
+        }
+    }
+};
+
+export default config;
