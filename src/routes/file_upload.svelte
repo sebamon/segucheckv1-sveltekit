7e93d43bc3ec99797eb2bd197814435e74c4e0e4
xTorettox <fg.garciacendra@gmail.com>
2021-10-11 20:08:46 -0300

    Added file_upload v1,uploads file w/preview

---
+++ src/routes/file_upload.svelte
@@ -0,0 +1,51 @@
+<script>
+    let avatar, fileInput;
+
+    const onFileSelected =(e)=>{
+        let image = e.target.files[0];
+            console.log(image);
+            let reader = new FileReader();
+            reader.readAsDataURL(image);
+            reader.onload = e => {
+                avatar = e.target.result
+            };
+    }
+</script>
+
+<div id="file_upload">
+    <h1>Subir una foto para el perfil del operario</h1>
+    <h2>Subir foto carnet de 184 x 184 px. Los formatos permitidos son JPG y PNG.</h2>
+    {#if avatar}
+    <img class="avatar" src="{avatar}" alt="foto de perfil">
+    {:else}
+    <img class="avatar" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png" alt="foto de perfil sin cargar" /> 
+    {/if}
+
+    <img class="upload" src="https://static.thenounproject.com/png/625182-200.png" alt="subir una foto" on:click="{()=>{fileInput.click();}}">
+    <div class="upload_button" on:click="{()=>{fileInput.click();}}">
+        Seleccionar imagen
+    </div>
+    <input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileInput} >
+
+</div>
+
+<style>
+	#file_upload{
+	display:flex;
+		align-items:center;
+		justify-content:center;
+		flex-flow:column;
+}
+ 
+	.upload{
+		display:flex;
+	    height:50px;
+		width:50px;
+		cursor:pointer;
+	}
+	.avatar{
+		display:flex;
+		height:200px;
+		width:200px;
+	}
+</style>
