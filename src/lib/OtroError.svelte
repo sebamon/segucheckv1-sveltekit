a9bd6d340eb71b4f2f4423423be3775f372f5fba
Arian <arian_eb@hotmail.com>
2021-10-13 00:26:40 -0300

    Fuentes agregadas, Home más detallado, Comienzo de páginas de errores, páginas vacías de panel (dashboard).

---
+++ src/lib/OtroError.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let error;
+	export let message;
+</script>
+
+<main class="container py-4">
+	<h1>{error} - Error diferente</h1>
+	<p class="lead">Detalle: {message}</p>
+</main>
+
+<footer class="container pt-5 my-5 text-muted border-top">
+	<p>
+		<strong>SeguCheck</strong> © 2021 - Desarrollado por
+		<a href="https://github.com/sebamon/segucheckv1-sveltekit">SanguCheck Dozen</a>
+	</p>
+</footer>
---
+++ src/routes/__error.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+	export function load({ error, status }) {
+		return {
+			props: {
+				message: error.message, // Descripción
+				error // Código
+			}
+		};
+	}
+</script>
+
+<script>
+	// REVISAR
+	import Error404 from '$lib/Error404.svelte'; // Página no encontrada
+	import OtroError from '$lib/OtroError.svelte'; // Otra página
+
+	export let error;
+	export let message;
+</script>
+
+{#if error == 404}
+	<!-- Used '==' instead of '===' to match string/number status code (just to be sure) -->
+	<Error404 {error} {message} />
+{:else}
+	<OtroError {error} {message} />
+{/if}
