e176f7c4d3e8e0ab6fc5a28f99b5452563b059b6
xTorettox <fg.garciacendra@gmail.com>
2021-10-13 19:26:01 -0300

    Minor changes to image handling

--- src/routes/file_upload.svelte
+++ src/routes/file_upload.svelte
@@ -1,51 +1,78 @@
 <script>
-    let avatar, fileInput;
+	let avatar, fileInput;
 
-    const onFileSelected =(e)=>{
-        let image = e.target.files[0];
-            console.log(image);
-            let reader = new FileReader();
-            reader.readAsDataURL(image);
-            reader.onload = e => {
-                avatar = e.target.result
-            };
-    }
+	const onFileSelected = (e) => {
+		let image = e.target.files[0];
+		console.log(image);
+		let reader = new FileReader();
+		reader.readAsDataURL(image);
+		reader.onload = (e) => {
+            avatar = e.target.result;
+		};
+        let imgNew = new Image();
+        imgNew = avatar;
+		imgNew.src = "avatar-usuario.png";
+	};
 </script>
 
 <div id="file_upload">
-    <h1>Subir una foto para el perfil del operario</h1>
-    <h2>Subir foto carnet de 184 x 184 px. Los formatos permitidos son JPG y PNG.</h2>
-    {#if avatar}
-    <img class="avatar" src="{avatar}" alt="foto de perfil">
-    {:else}
-    <img class="avatar" src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png" alt="foto de perfil sin cargar" /> 
-    {/if}
+	<h1>Subir una foto para el perfil del operario</h1>
+	<h2>Subir foto carnet de 184 x 184 px. Los formatos permitidos son JPG y PNG.</h2>
+	{#if avatar}
+		<img class="avatar" src={avatar} alt="foto de perfil" />
+	{:else}
+		<img
+			class="avatar"
+			src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png"
+			alt="foto de perfil sin cargar"
+		/>
+	{/if}
 
-    <img class="upload" src="https://static.thenounproject.com/png/625182-200.png" alt="subir una foto" on:click="{()=>{fileInput.click();}}">
-    <div class="upload_button" on:click="{()=>{fileInput.click();}}">
-        Seleccionar imagen
-    </div>
-    <input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileInput} >
-
+	<img
+		class="upload"
+		src="https://static.thenounproject.com/png/625182-200.png"
+		alt="subir una foto"
+		on:click={() => {
+			fileInput.click();
+		}}
+	/>
+	<div
+		class="upload_button"
+		on:click={() => {
+			fileInput.click();
+		}}
+	>
+		Seleccionar imagen
+	</div>
+	<input
+		style="display:none"
+		type="file"
+		accept=".jpg, .jpeg, .png"
+		on:change={(e) => onFileSelected(e)}
+		bind:this={fileInput}
+	/>
+</div>
+<div id="probando_img">
+    <img src="{"avatar-usuario.png"}" alt="">
 </div>
 
 <style>
-	#file_upload{
-	display:flex;
-		align-items:center;
-		justify-content:center;
-		flex-flow:column;
-}
- 
-	.upload{
-		display:flex;
-	    height:50px;
-		width:50px;
-		cursor:pointer;
+	#file_upload {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-flow: column;
 	}
-	.avatar{
-		display:flex;
-		height:200px;
-		width:200px;
+
+	.upload {
+		display: flex;
+		height: 50px;
+		width: 50px;
+		cursor: pointer;
+	}
+	.avatar {
+		display: flex;
+		height: 184px;
+		width: 184px;
 	}
 </style>
