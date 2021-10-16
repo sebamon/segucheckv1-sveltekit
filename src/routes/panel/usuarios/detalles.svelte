0ab2f14fe0bfa5b8234a63214458a8c391a97cb2
Arian <arian_eb@hotmail.com>
2021-10-16 20:01:10 -0300

    Select de roles asignados en detalles de usuario.

--- src/routes/panel/usuarios/detalles.svelte
+++ src/routes/panel/usuarios/detalles.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+import { each } from 'svelte/internal';
+
 	// Importar por nombre de componentes: https://sveltestrap.js.org/
 	import {
 		Button,
@@ -28,17 +30,21 @@
 		nationality: 'Argentina',
 		studyLevel: 'Universitario completo',
 		degree: 'Licenciado',
-		profilePic: 'https://avatars.dicebear.com/api/micah/1234.svg'
+		profilePic: 'https://avatars.dicebear.com/api/micah/1234.svg',
+		roles: [
+			{ rol_id: 1, rolDescription: 'Gestor documental' },
+			{ rol_id: 2, rolDescription: 'Personal de seguridad' }
+		]
 	};
 	// Pasar inicial de género a texto legible:
 	let genderText;
-	if (userDetails.gender == "M") {
-		genderText = "Varón";
-	} else if (userDetails.gender == "F") {
-		genderText = "Mujer";
+	if (userDetails.gender == 'M') {
+		genderText = 'Varón';
+	} else if (userDetails.gender == 'F') {
+		genderText = 'Mujer';
 	} else {
 		genderText = userDetails.gender;
-	};
+	}
 </script>
 
 <svelte:head>
@@ -58,7 +64,14 @@
 		<BreadcrumbItem active>Detalles</BreadcrumbItem>
 	</Breadcrumb>
 	<div class="col-md-3">
-		<Image fluid thumbnail src="{userDetails.profilePic}" alt="Aplicación móvil" class="m-2" style="max-width:200px" />
+		<Image
+			fluid
+			thumbnail
+			src={userDetails.profilePic}
+			alt="Aplicación móvil"
+			class="m-2"
+			style="max-width:200px"
+		/>
 	</div>
 	<div class="col-auto">
 		<h1>{userDetails.name + ' ' + userDetails.lastName}</h1>
@@ -76,55 +89,173 @@
 	<div class="row mb-3 g-3">
 		<div class="col-md-6">
 			<label for="user_id" class="form-label">Número ID</label>
-			<input type="text" id="user_id" name="user_id" class="form-control" placeholder="1234" aria-label="Número ID" value="{userDetails.user_id}" readonly/>
+			<input
+				type="text"
+				id="user_id"
+				name="user_id"
+				class="form-control"
+				placeholder="1234"
+				aria-label="Número ID"
+				value={userDetails.user_id}
+				readonly
+			/>
 		</div>
 		<div class="col-md-6">
 			<label for="cuit" class="form-label">Número CUIT</label>
-			<input type="text" id="cuit" name="cuit" class="form-control" placeholder="20301001008" aria-label="Número CUIT" value="{userDetails.cuit}" readonly/>
+			<input
+				type="text"
+				id="cuit"
+				name="cuit"
+				class="form-control"
+				placeholder="20301001008"
+				aria-label="Número CUIT"
+				value={userDetails.cuit}
+				readonly
+			/>
 		</div>
 	</div>
 	<div class="row mb-3 g-3">
 		<div class="col-md-6">
 			<label for="name" class="form-label">Nombre</label>
-			<input type="text" id="name" name="name" class="form-control" placeholder="Juan" aria-label="Nombre" value="{userDetails.name}" readonly/>
+			<input
+				type="text"
+				id="name"
+				name="name"
+				class="form-control"
+				placeholder="Juan"
+				aria-label="Nombre"
+				value={userDetails.name}
+				readonly
+			/>
 		</div>
 		<div class="col-md-6">
 			<label for="lastName" class="form-label">Apellido</label>
-			<input type="text" id="lastName" name="lastName" class="form-control" placeholder="Perez" aria-label="First name" value="{userDetails.lastName}" readonly/>
+			<input
+				type="text"
+				id="lastName"
+				name="lastName"
+				class="form-control"
+				placeholder="Perez"
+				aria-label="First name"
+				value={userDetails.lastName}
+				readonly
+			/>
 		</div>
 	</div>
 	<div class="row mb-3 g-3">
 		<div class="col-md-6">
 			<label for="email" class="form-label">Correo electrónico</label>
-			<input type="text" id="email" name="email" class="form-control" placeholder="Juan" aria-label="Correo electrónico" value="{userDetails.email}" readonly/>
+			<input
+				type="text"
+				id="email"
+				name="email"
+				class="form-control"
+				placeholder="Juan"
+				aria-label="Correo electrónico"
+				value={userDetails.email}
+				readonly
+			/>
 		</div>
 		<div class="col-md-6">
 			<label for="phone" class="form-label">Teléfono</label>
-			<input type="text" id="phone" name="phone" class="form-control" placeholder="2993334444" aria-label="Teléfono" value="{userDetails.phone}" readonly/>
+			<input
+				type="text"
+				id="phone"
+				name="phone"
+				class="form-control"
+				placeholder="2993334444"
+				aria-label="Teléfono"
+				value={userDetails.phone}
+				readonly
+			/>
 		</div>
 	</div>
 	<div class="row mb-3 g-3">
 		<div class="col-md-4">
-			<label for="lastName" class="form-label"><small>Género</label>
-			<input type="text" id="lastName" name="lastName" class="form-control user-select-all" placeholder="Perez" aria-label="Género" value="{genderText}" readonly/>
+			<label for="lastName" class="form-label"><small>Género</small></label>
+			<input
+				type="text"
+				id="lastName"
+				name="lastName"
+				class="form-control user-select-all"
+				placeholder="Perez"
+				aria-label="Género"
+				value={genderText}
+				readonly
+			/>
 		</div>
 		<div class="col-md-4">
 			<label for="name" class="form-label">Fecha de nacimiento</label>
-			<input type="text" id="name" name="name" class="form-control" placeholder="Juan" aria-label="Fecha de nacimiento" value="{userDetails.dateOfBirth}" readonly/>
+			<input
+				type="text"
+				id="name"
+				name="name"
+				class="form-control"
+				placeholder="Juan"
+				aria-label="Fecha de nacimiento"
+				value={userDetails.dateOfBirth}
+				readonly
+			/>
 		</div>
 		<div class="col-md-4">
 			<label for="lastName" class="form-label">Nacionalidad</label>
-			<input type="text" id="lastName" name="lastName" class="form-control" placeholder="Perez" aria-label="Nacionalidad" value="{userDetails.nationality}" readonly/>
+			<input
+				type="text"
+				id="lastName"
+				name="lastName"
+				class="form-control"
+				placeholder="Perez"
+				aria-label="Nacionalidad"
+				value={userDetails.nationality}
+				readonly
+			/>
 		</div>
 	</div>
 	<div class="row mb-3 g-3">
 		<div class="col-md-6">
 			<label for="studyLevel" class="form-label">Nivel de formación</label>
-			<input type="text" id="studyLevel" name="studyLevel" class="form-control" placeholder="Universitario completo" aria-label="Correo electrónico" value="{userDetails.studyLevel}" readonly/>
+			<input
+				type="text"
+				id="studyLevel"
+				name="studyLevel"
+				class="form-control"
+				placeholder="Universitario completo"
+				aria-label="Correo electrónico"
+				value={userDetails.studyLevel}
+				readonly
+			/>
 		</div>
 		<div class="col-md-6">
 			<label for="degree" class="form-label">Título de formación</label>
-			<input type="text" id="degree" name="degree" class="form-control" placeholder="Licenciado" aria-label="Título de formación" value="{userDetails.degree}" readonly/>
+			<input
+				type="text"
+				id="degree"
+				name="degree"
+				class="form-control"
+				placeholder="Licenciado"
+				aria-label="Título de formación"
+				value={userDetails.degree}
+				readonly
+			/>
+		</div>
+	</div>
+	<div class="row mb-3 g-3">
+		<div class="col-md-6">
+			<label for="roles" class="form-label">Roles asignados</label>
+			<select
+				id="roles"
+				name="roles"
+				class="form-control overflow-visible"
+				aria-label="Roles"
+				value={userDetails.degree}
+				multiple>
+			{#if (userDetails.roles.length === 0)}
+				<option selected>Ninguno</option>
+			{:else}
+				{#each userDetails.roles as {rol_id, rolDescription}}
+					<option value="{rol_id}">{rolDescription}</option>
+				{/each}
+			{/if}
 		</div>
 	</div>
 </form>
