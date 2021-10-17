ca2ac22b9bebc4ed2e6646a874f83d3394e81f5f
Arian <arian_eb@hotmail.com>
2021-10-17 20:41:38 -0300

    Nuevo trabajo creado. Componente VehicleDetails en proceso.

---
+++ src/lib/Details/VehicleDetails.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	// Datos del usuario a mostrar
+	export let user_id = 0;
+	export let cuit = 0;
+	export let firstName = '';
+	export let lastName = '';
+	export let email = '';
+	export let phone = '';
+	export let gender = '';
+	export let dateOfBirth = '';
+	export let nationality = '';
+	export let studyLevel = '';
+	export let degree = '';
+	export let roles = [];
+
+	// Pasar inicial de género a texto legible:
+	let genderText;
+	if (gender == 'M') {
+		genderText = 'Varón';
+	} else if (gender == 'F') {
+		genderText = 'Mujer';
+	} else {
+		genderText = gender;
+	}
+</script>
