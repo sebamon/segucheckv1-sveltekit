fad1308c515c5a3cee658a841fa3928ba4f180df
xTorettox <fg.garciacendra@gmail.com>
2021-10-19 21:21:26 -0300

    Corregidos detalles menores, eliminados archivos innecesarios. No carga archivos.

--- src/routes/file_upload.svelte
+++ src/routes/file_upload.svelte
@@ -1,5 +1,35 @@
 <script>
-import Pdf_file_upload from "./Pdf_file_upload.svelte";
-
+	import Pdf_file_upload from './Pdf_file_upload.svelte';
+	import ImgUpload from './Img_upload.svelte';
 </script>
-<Pdf_file_upload />
+
+<div class="container">
+	<div class="upload_img">
+		<ImgUpload />
+	</div>
+	<div class="upload_file">
+		<Pdf_file_upload />
+	</div>
+</div>
+
+<style>
+	.container {
+        width: 80%;
+        height: 80%;
+        align-self: center;
+    }
+    .upload_img {
+		width: 50%;
+		height: 30%;
+		align-self:right;
+		color: white;
+		background-color: lightblue;
+	}
+	.upload_file {
+		width: 50%;
+		height: 30%;
+		align-self: right;
+		color: white;
+		background-color: palevioletred;
+	}
+</style>
