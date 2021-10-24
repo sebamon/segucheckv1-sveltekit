5b0ba56b7a4b583d7cb5e9f9afe98b07079362a0
Arian <arian_eb@hotmail.com>
2021-10-23 22:19:04 -0300

    Iconito de casa en título Resumen, y links con alineación derecha.

--- src/routes/panel/index.svelte
+++ src/routes/panel/index.svelte
@@ -1,16 +1,6 @@
 <script lang="ts">
 	// Importar por nombre de componentes: https://sveltestrap.js.org/
-	import {
-		Breadcrumb,
-		BreadcrumbItem,
-		Card,
-		CardBody,
-		CardFooter,
-		CardHeader,
-		CardSubtitle,
-		CardText,
-		CardTitle
-	} from 'sveltestrap';
+	import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardSubtitle, CardTitle } from 'sveltestrap';
 
 	// Datos de usuario - Ver si esto lo maneja un hook
 	let actualUser = 'Juan Perez';
@@ -22,11 +12,11 @@
 
 <!-- Encabezado -->
 <header>
-<Breadcrumb>
-	<BreadcrumbItem active>Inicio</BreadcrumbItem>
-</Breadcrumb>
-<h1>Resumen</h1>
-<p class="lead">Bienvenido de nuevo, {actualUser}</p>
+	<Breadcrumb>
+		<BreadcrumbItem active>Inicio</BreadcrumbItem>
+	</Breadcrumb>
+	<h1><i class="fas fa-home me-4" />Resumen</h1>
+	<p class="lead">Bienvenido de nuevo, {actualUser}</p>
 </header>
 
 <main class="row g-2">
@@ -39,9 +29,11 @@
 			</div>
 			<CardTitle class="fw-bold">Próximos vencimientos en operarios</CardTitle>
 			<CardSubtitle>Operarios con documentación a expirar en menos de una semana</CardSubtitle>
-			<a href="/panel/operarios?filter=vencePronto" class="text-decoration-none fw-bold">
-				Ver operarios <i class="fas fa-arrow-right me-2" />
-			</a>
+			<p class="text-end">
+				<a href="/panel/operarios?filter=vencePronto" class="text-decoration-none fw-bold">
+					Ver operarios <i class="fas fa-arrow-right me-2" />
+				</a>
+			</p>
 		</CardBody>
 	</Card>
 	<Card class="col-xl-3 col-md-6">
@@ -53,9 +45,11 @@
 			</div>
 			<CardTitle class="fw-bold">Próximos vencimientos en vehículos</CardTitle>
 			<CardSubtitle>Vehículos con documentación a expirar en menos de una semana</CardSubtitle>
-			<a href="/panel/vehiculos?filter=vencePronto" class="text-decoration-none fw-bold">
-				Ver vehículos <i class="fas fa-arrow-right me-2" />
-			</a>
+			<p class="text-end">
+				<a href="/panel/vehiculos?filter=vencePronto" class="text-decoration-none fw-bold">
+					Ver vehículos <i class="fas fa-arrow-right me-2" />
+				</a>
+			</p>
 		</CardBody>
 	</Card>
 	<Card class="col-xl-3 col-md-6">
@@ -67,9 +61,11 @@
 			<CardSubtitle>
 				Puede registrar nuevos usuarios, modificar sus datos o realizar la baja de los mismos
 			</CardSubtitle>
-			<a href="/info/plataforma" class="text-decoration-none fw-bold">
-				Gestionar usuarios <i class="fas fa-arrow-right me-2" />
-			</a>
+			<p class="text-end">
+				<a href="/info/plataforma" class="text-decoration-none fw-bold">
+					Gestionar usuarios <i class="fas fa-arrow-right me-2" />
+				</a>
+			</p>
 		</CardBody>
 	</Card>
 	<Card class="col-xl-3 col-md-6">
@@ -79,9 +75,11 @@
 			</div>
 			<CardTitle class="fw-bold">Historial de trabajos</CardTitle>
 			<CardSubtitle>Últimas órdenes de trabajo vigentes</CardSubtitle>
-			<a href="/info/plataforma" class="text-decoration-none fw-bold">
-				Ver historial <i class="fas fa-arrow-right me-2" />
-			</a>
+			<p class="text-end">
+				<a href="/info/plataforma" class="text-decoration-none fw-bold">
+					Ver historial <i class="fas fa-arrow-right me-2" />
+				</a>
+			</p>
 		</CardBody>
 	</Card>
 </main>
