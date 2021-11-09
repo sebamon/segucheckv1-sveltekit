be4043945dc1b6893527308b7a28d6075cbcb9c3
Arian <arian_eb@hotmail.com>
2021-11-08 22:05:02 -0300

    Bis

--- src/routes/panel/operarios/index.svelte
+++ src/routes/panel/operarios/index.svelte
@@ -1,41 +1,39 @@
 <script context="module">
-	export async function load ({fetch , page}){
-		try{
-
-			const response = await fetch('./operarios/operarios',{
-				method : "GET",
+	export async function load({ fetch, page }) {
+		try {
+			const response = await fetch('./operarios/operarios', {
+				method: 'GET',
 				headers: {
-					'Content-Type': 'application/json',
-				},
-			})
-			const data = await response.json()
-			console.log('data',data)
+					'Content-Type': 'application/json'
+				}
+			});
+			const data = await response.json();
+			console.log('data', data);
 			return {
 				props: {
 					data
 				}
-			}
-		}catch(e){
-			console.log('error',e)
+			};
+		} catch (e) {
+			console.log('error', e);
 			return {
-				props:
-					{}
-			}
+				props: {}
+			};
 		}
-}
+	}
 </script>
+
 <script lang="ts">
 	// Importar por nombre de componentes: https://sveltestrap.js.org/
-	import { Button, Breadcrumb, BreadcrumbItem , Alert} from 'sveltestrap';
-	export let data
-	export let operators = data.operators
+	import { Button, Breadcrumb, BreadcrumbItem, Alert } from 'sveltestrap';
+	export let data;
+	export let operators = data.operators;
 
-
-	interface user  {
-		user_id : number,
-		firstName : string,
-		lastName : string,
-		cuit : string,
+	interface user {
+		user_id: number;
+		firstName: string;
+		lastName: string;
+		cuit: string;
 	}
 </script>
 
@@ -63,56 +61,70 @@
 </header>
 
 <main>
-	{#if data.status==='INFO'}
-		<Alert color='secondary'> 
+	{#if data.status === 'INFO'}
+		<Alert color="secondary">
 			<h4 class="alert-heading text-capitalize">Atención:</h4>
 			<p>{data.message}</p>
 		</Alert>
-	{:else}
-	{#if operators.length>0}
-	<div class="table-responsive">
-		<table class="table table-striped table-hover align-middle">
-			<thead>
-				<tr>
-					<th scope="col"><input type="search" placeholder="Filtrar" /></th>
-					<th scope="col"><input type="search" placeholder="Filtrar" /></th>
-					<th scope="col"><input type="search" placeholder="Filtrar" /></th>
-					<th scope="col"><input type="search" placeholder="Filtrar" /></th>
-					<th scope="col"><input type="search" placeholder="Filtrar" /></th>
-				</tr>
-				<tr>
-					<th scope="col">ID Cuenta</th>
-					<th scope="col">DNI</th>
-					<th scope="col">Nombre</th>
-					<th scope="col">Apellido</th>
-					<th scope="col">Estado Habilitación</th>
-				</tr>
-			</thead>
+	{:else if operators.length > 0}
+		<div class="table-responsive">
+			<table class="table table-striped table-hover align-middle">
+				<thead>
+					<tr>
+						<th scope="col"><input type="search" placeholder="Filtrar" /></th>
+						<th scope="col"><input type="search" placeholder="Filtrar" /></th>
+						<th scope="col"><input type="search" placeholder="Filtrar" /></th>
+						<th scope="col"><input type="search" placeholder="Filtrar" /></th>
+						<th scope="col"><input type="search" placeholder="Filtrar" /></th>
+					</tr>
+					<tr>
+						<th scope="col">ID Cuenta</th>
+						<th scope="col">DNI</th>
+						<th scope="col">Nombre</th>
+						<th scope="col">Apellido</th>
+						<th scope="col">Estado Habilitación</th>
+					</tr>
+				</thead>
 				<tbody>
 					{#each operators as operator}
 						<tr>
 							<td>
-								<a class="text-decoration-none text-dark" href="./operarios/{operator.users.user_id}">
+								<a
+									class="text-decoration-none text-dark"
+									href="./operarios/{operator.users.user_id}"
+								>
 									{operator.users.user_id}
 								</a>
 							</td>
 							<td>
-								<a class="text-decoration-none text-dark" href="./operarios/{operator.users.user_id}">
+								<a
+									class="text-decoration-none text-dark"
+									href="./operarios/{operator.users.user_id}"
+								>
 									{operator.users.cuit}
 								</a>
 							</td>
 							<td>
-								<a class="text-decoration-none text-dark" href="./operarios/{operator.users.user_id}">
+								<a
+									class="text-decoration-none text-dark"
+									href="./operarios/{operator.users.user_id}"
+								>
 									{operator.users.firstName}
 								</a>
 							</td>
 							<td>
-								<a class="text-decoration-none text-dark" href="./operarios/{operator.users.user_id}">
+								<a
+									class="text-decoration-none text-dark"
+									href="./operarios/{operator.users.user_id}"
+								>
 									{operator.users.lastName}
 								</a>
 							</td>
 							<td>
-								<a class="text-decoration-none text-dark" href="./operarios/{operator.users.user_id}">
+								<a
+									class="text-decoration-none text-dark"
+									href="./operarios/{operator.users.user_id}"
+								>
 									Habilitado
 								</a>
 							</td>
@@ -121,6 +133,5 @@
 				</tbody>
 			</table>
 		</div>
-	{/if}
 	{/if}
 </main>
