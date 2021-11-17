049222e839418d06417311d776f37c1548b28ee5
Arian <arian_eb@hotmail.com>
2021-11-16 23:36:25 -0300

    Intentando hacer andar fetch QR

--- src/routes/app/qr/index.svelte
+++ src/routes/app/qr/index.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import CopyToClipboard from 'svelte-copy-to-clipboard';
-    import { Toast } from 'sveltestrap';
-    let isOpen = false;
+	import { Toast, Alert } from 'sveltestrap';
+	let isOpen = false;
 
 	// Datos del usuario a mostrar (reemplazar con fetch):
 	let userDetails = {
@@ -10,11 +10,66 @@
 		firstName: 'Juan',
 		lastName: 'Perez'
 	};
-	// Link + hash acceso temporal:
+	// Generar link + hash acceso temporal:
 	let urlToProfile = 'http://localhost:3000/app/perfil?auth=1234';
 
+	async function getQR() {
+		//funcion que toma los datos del formulario y lo envia por metodo post
+		const response = await fetch('https://qrcode3.p.rapidapi.com/qrcode/text', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+				'x-rapidapi-host': 'qrcode3.p.rapidapi.com',
+				'x-rapidapi-key': 'a0ed585700msh47277d8980e0c51p169d27jsnb5d5fe25b530'
+			},
+			body: {
+				data: urlToProfile,
+				image: {
+					uri: 'https://i.imgur.com/QYfrqU1.png',
+					modules: false
+				},
+				style: {
+					module: {
+						color: '#32155C',
+						shape: 'lightround'
+					},
+					inner_eye: {
+						shape: 'lightround'
+					},
+					outer_eye: {
+						shape: 'lightround'
+					}
+				},
+				size: {
+					width: 500,
+					quiet_zone: 2,
+					error_correction: 'M'
+				},
+				output: {
+					filename: 'qrcode',
+					format: 'png'
+				}
+			}
+		})
+			.then((response) => {
+				console.log(response);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+		return await response.blob();
+		/* try {
+			
+		} catch (err) {
+            console.log("Esto falló :(");
+		} */
+	}
+
+	let urlQRpic = getQR();
+
+	// Alternar mensaje en Toast:
 	const copied = () => {
-        isOpen = true;
+		isOpen = true;
 	};
 </script>
 
@@ -28,17 +83,33 @@
 </header>
 <main>
 	<div class="row justify-content-center">
-		<img src="/img/qr-code.png" class="img-fluid" style="max-width:500px" alt="Código QR" title="¡Escanea este código!" />
+		{#await getQR()}
+			<Alert color="secondary">
+				<i class="fas fa-spinner fa-pulse fs-1 fst-italic me-2" />Cargando QR...
+			</Alert>
+		{:then urlQRpic}
+			<img
+				src={urlQRpic}
+				class="img-fluid"
+				style="max-width:500px"
+				alt="Código QR"
+				title="¡Escanea este código!"
+			/>
+		{:catch err}
+			<Alert color="secondary">
+				<h4 class="alert-heading text-capitalize">Atención:</h4>
+				<p>{err}</p>
+			</Alert>
+		{/await}
 	</div>
 	<div class="row">
 		<CopyToClipboard text={urlToProfile} on:copy={copied} let:copy>
 			<button class="btn btn-primary" id="buttonCopy" on:click={copy}>
-                Copiar enlace al perfil
-            </button>
+				Copiar enlace al perfil
+			</button>
 		</CopyToClipboard>
-            <Toast {isOpen} autohide body on:close={() => (isOpen = false)} class="bg-light">
-                <span class="fw-bold"><i class="fas fa-check text-primary mx-2" />Texto copiado!</span>
-            </Toast>
-        
+		<Toast {isOpen} autohide body on:close={() => (isOpen = false)} class="bg-light">
+			<span class="fw-bold"><i class="fas fa-check text-primary mx-2" />Texto copiado!</span>
+		</Toast>
 	</div>
 </main>
