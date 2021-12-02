e753946b976fb5de9eff396ff3c69901cb048643
xTorettox <fg.garciacendra@gmail.com>
2021-12-02 15:31:48 -0300

    Cambios sobre el control de bindeos

--- src/lib/Checklists.svelte
+++ src/lib/Checklists.svelte
@@ -1,116 +1,250 @@
-<script>
-	/* Valores demo */
-	let nombreCheck = 'Control de Camionetas - Salida';
-	let nombreCheckGroup = '';
-	let optionsCheck = [
-		{ id: '1', value: 'Control Vehicular' },
-		{ id: '2', value: 'Control Salud' }
-	];
-	let selectedOption = 1;
-	let item1 = 'Verificar que la carga del extintor esté completa';
-	let item2 = 'Controlar que la presión de los neumáticos sea adecuada';
+<script lang="ts">
+	// Requerimientos para el modal
+	import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'sveltestrap';
 
-	/* Definimos los elementos de la checklist */
-	const checkItem = {
-		id: '',
-		item: '',
-		description: '',
+	/* Definimos los tipos de la checklist */
+	type checkItem = {
+		checkItem_id: number;
+		item: string;
+		description: string;
+		categories: checkcategory[];
+	}; //Listo model checkitem
+
+	type checkcategory = {
+		category_id: number;
+		categoryName: string;
+		checkItems: checkItem[];
+	}; //Listo model checkcategory
+
+	type verifyItem = {
+		verifyItem_id: number;
+		checkItem: checkItem;
+		checked: boolean;
+		observation: string;
+	}; //Listo model verifyItem
+
+	type verify = {
+		verify_id: number;
+		list: verifyItem[];
 	};
 
-    const checkCategory = {
-        id: '',
-        name: '',
-        checkItems: [] // arreglo de checkItems
-    };
+	type checkList = {
+		checkList_id: number;
+		checkListName: string;
+		verify: verify;
+	};
 
-    const verify = {
-        id: '',
-        list: [] // arreglo de verifyItems
-    };
-
-    const verifyItem = {
-        checkItem: '', // checkItem
-        checked: false,
-        observation: '',
-    };
-
-    const checkList = {
-       checkList_id    : '',
-        verify          : '' // verify
-    }
+	/* Modales!! */
+	let open = false;
+	const toggle = () => {
+		open = !open;
+	};
 
 	// Funciones para agregar o quitar ítems de la checklist
-	const addItem = () => {};
+	const addItem = () => {
+		let newItem: checkItem;
+		let checkItem_id: number;
+		let item: string;
+		let description: string;
+		let categories: checkcategory[];
+
+		return `<label for="">Mock item  </label><input type="checkbox" /> Mock item`;
+	};
 	const substractItem = () => {};
+
+	// Funciones para agregar o quitar categorías de la checklist
+	let categoryCollection = [];
+	let categoryIndex = categoryCollection.length;
+	let nombreCheckCategory = '';
+
+	const editCatName = (element) => {
+		console.log('Mi elemento dentro de editCatName: ', element);
+		let catId = element.split('_').pop();
+		catId = catId.split('"');
+
+		console.log('catId', catId);
+		document.getElementById(catId[0]).innerHTML = 'Nombre nuevo';
+	};
+	
+	const addCategory = () => {
+
+		// Renombrar categoría
+		document.addEventListener('click', function (e) {
+			if (e.target) {
+				console.log("Mi e: ", e);
+				// console.log("Mi id: ", JSON.stringify(e.path[0].id));
+				// editCatName(JSON.stringify(e.path[1]));
+				editCatName(JSON.stringify(e.path[0].id));
+				//e.target.
+			}
+		});
+
+		let newCategoryDivForm = document.createElement('div');
+		let newCategoryDivDemo = document.createElement('div');
+		let newItemDiv = document.createElement('div');
+
+		let category_id = categoryIndex;
+		let categoryName = nombreCheckCategory;
+		let checkItems = [];
+		let newCategory: checkcategory = { category_id, categoryName, checkItems };
+		categoryCollection.push(newCategory);
+
+		// Creo el elemento para el formulario
+		let catDomId = 'category-' + newCategory.category_id + 1;
+		newCategoryDivForm.setAttribute('id', catDomId);
+		newCategoryDivForm.innerHTML = `<h5>${newCategory.category_id + 1} - ${
+			newCategory.categoryName
+		}</h5><Button id="btn_${catDomId}">Botón</Button><br />
+				<input type="text bind:value="${newCategory.categoryName}" placeholder="${
+			newCategory.category_id + 1
+		} - ${newCategory.categoryName}" />`;
+		// newCategoryDivForm.innerHTML = `<h5>${newCategory.category_id+1} - ${newCategory.categoryName}</h5>`;
+
+		newItemDiv.setAttribute('id', 'item-container-' + categoryIndex);
+		newItemDiv.innerHTML = addItem();
+		newCategoryDivForm.appendChild(newItemDiv);
+
+		document.getElementById('itemsContainer').appendChild(newCategoryDivForm);
+
+		// Creo el elemento para la vista previa
+		newCategoryDivDemo.setAttribute('id', 'category-demo-' + categoryIndex);
+		newCategoryDivDemo.innerHTML = `<h5>${categoryIndex + 1} - ${nombreCheckCategory}</h5>`;
+		document.getElementById('itemsContainerDemo').appendChild(newCategoryDivDemo);
+		// Parámetros finales
+		categoryIndex += 1;
+		nombreCheckCategory = '';
+		toggle();
+	};
+
+	const substractCategory = () => {};
+	const addButtons = () => {};
+	const substractButtons = () => {};
+
+	/* ************************ Valores demo ************************ */
+	let nombreCheck = 'Control de Camionetas - Salida';
+	let optionsCheck = [
+		{
+			id: '1',
+			item: 'Control Vehicular',
+			description: 'Verificar que la carga del extintor esté completa'
+		},
+		{
+			id: '2',
+			item: 'Control Salud',
+			description: 'Controlar que la presión de los neumáticos sea adecuada'
+		}
+	];
+	let selectedOption = 1;
+	/* ************************ Valores demo ************************ */
 </script>
 
-<div id="conteiner">
+<svelte:head>
+	<link
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+		rel="stylesheet"
+		integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+		crossorigin="anonymous"
+	/>
+	<script
+		src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+		crossorigin="anonymous"></script>
+</svelte:head>
+
+<div id="conteiner" class="row justify-content-center">
 	<!-- Formulario nuevo checklist -->
-	<div id="formCheck" class="col">
-		<h4>Datos de la Checklist</h4>
-		<label for="checklistName">Nombre de la Checklist</label><br />
+	<div id="formCheck" class="col-6">
+		<h2>Datos de la Checklist</h2>
+		<label for="checklistName">Nombre</label><br />
 		<input type="text" placeholder="Control de Camionetas - Salida" bind:value={nombreCheck} /><br
 		/>
-		<label for="checklistGroup">Grupo</label><br />
-		<!-- <select name="checkGroup" bind:value={nombreCheckGroup}>
-            <option value="1">Control Vehicular</option>
-            <option value="2">Control Salud</option>
-        </select><br /> -->
+
+		<!--<label for="checklistGroup">Grupo</label><br />
 		<select bind:value={selectedOption}>
 			{#each optionsCheck as option}
-				{#if (option.id = 1)}
-					<option value={option.id} id={option.id} selected>{option.value}</option>
-				{:else}
-					<option value={option.id} id={option.id}>{option.value}</option>
-				{/if}
+				<option value={option.id} id={option.id}>{option.item}</option>
 			{/each}
-		</select>
+		</select> -->
+
+		<Button id="addCategory" on:click={toggle}>
+			<img src="/img/add.png" alt="agregar categoría" id="tinyAdd" />
+			Agregar categoría</Button
+		>
+		<Button id="substractCategory" on:click={substractCategory}>
+			<img src="/img/sub.png" alt="remover categoría" id="tinySubs" />
+		</Button>
 
 		<h4>Items</h4>
 
+		<!-- Testeando ids dinámicos -->
+		<div id="itemsContainer">
+			<!-- Acá se agregan las categorías -->
+		</div>
 		<label for="checklistItem-1">Item 1</label><br />
 		<input id="checklistItem-1" type="text" placeholder="Control Carga Extintor" /><br />
 		<label for="checklistItemDescription-1">Descripción ítem 1</label><br />
-		<input id="checklistItemDescription-1" type="text" placeholder={item1} bind:value={item1} /><br
-		/>
+		<input
+			id="checklistItemDescription-1"
+			type="text"
+			placeholder={optionsCheck[0].description}
+			bind:value={optionsCheck[0].description}
+		/><br />
 
 		<label for="checklistItem-2">Item 2</label><br />
 		<input id="checklistItem-2" type="text" placeholder="Control Presión de Neumáticos" /><br />
 		<label for="checklistItemDescription-2">Descripción ítem 2</label><br />
-		<input id="checklistItemDescription-2" type="text" placeholder={item2} bind:value={item2} /><br
-		/>
-		<button id="addItem">
-			<img src="/static/img/button-add.png" alt="agregar ítem" id="tinyAdd" />
-			Agregar ítem</button
-		>
-        <button id="substractItem">
-			<img src="/static/img/button-subs.png" alt="remover ítem" id="tinySubs" />
-			Remover ítem</button
-		>
+		<input
+			id="checklistItemDescription-2"
+			type="text"
+			placeholder={optionsCheck[1].description}
+			bind:value={optionsCheck[1].description}
+		/><br />
+		<Button id="addItem">
+			<img src="/img/add.png" alt="agregar ítem" id="tinyAdd" />
+		</Button>
+		<Button id="substractItem">
+			<img src="/img/sub.png" alt="remover ítem" id="tinySubs" />
+		</Button>
 	</div>
 
-	<div id="vistaPrevia" class="col">
+	<div id="vistaPrevia" class="col-6">
 		<h2>Vista Previa</h2>
 		<form name="demo">
 			<h4>{nombreCheck}</h4>
-			<h5>{selectedOption} - {optionsCheck[selectedOption - 1].value}</h5>
-			<input type="checkbox" />
-			{item1} <br />
-			<input type="checkbox" />
-			{item2} <br />
+			<h5>{selectedOption} - {optionsCheck[selectedOption - 1].item}</h5>
+			<div id="itemsContainerDemo">
+				<input type="checkbox" />
+				{optionsCheck[0].description} <br />
+				<input type="checkbox" />
+				{optionsCheck[1].description} <br />
+			</div>
 		</form>
+	</div>
+	<!-- Modal Categoría -->
+	<div>
+		<Modal isOpen={open} {toggle}>
+			<ModalHeader {toggle}>Agregar categoría</ModalHeader>
+			<ModalBody>
+				<input type="text" bind:value={nombreCheckCategory} placeholder="ingresar un nombre" />
+			</ModalBody>
+			<ModalFooter>
+				<Button color="primary" on:click={addCategory}>Agregar</Button>
+				<Button color="secondary" on:click={toggle}>Cancelar</Button>
+			</ModalFooter>
+		</Modal>
 	</div>
 </div>
 
 <style>
 	input[type='text'],
 	select {
-		width: 50%;
+		width: 100%;
 	}
 	input[placeholder] {
 		text-decoration-color: darkgray;
 	}
-	#tinyAdd, tinySubs {
+	#tinyAdd,
+	#tinySubs {
 		width: 22px;
 		height: 22px;
 	}
