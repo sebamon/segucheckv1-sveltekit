e2dcc5f86c444eca479e58ec224e6ddf9151fc64
xTorettox <fg.garciacendra@gmail.com>
2021-12-03 01:32:14 -0600

    Manejo dinámico de elementos. En proceso.

--- src/lib/Checklists.svelte
+++ src/lib/Checklists.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	// Requerimientos para el modal
 	import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'sveltestrap';
+	import { onMount } from 'svelte';
+
 
 	/* Definimos los tipos de la checklist */
 	type checkItem = {
@@ -41,14 +43,31 @@
 	};
 
 	// Funciones para agregar o quitar ítems de la checklist
-	const addItem = () => {
+	const addItem = (id) => {
 		let newItem: checkItem;
 		let checkItem_id: number;
 		let item: string;
 		let description: string;
 		let categories: checkcategory[];
+		let currentCategory: checkcategory = categoryCollection[id - 1];
 
-		return `<label for="">Mock item  </label><input type="checkbox" /> Mock item`;
+		checkItem_id = currentCategory.checkItems.length;
+
+		document.getElementById(
+			`category-${id}`
+		).innerHTML += `<br /><input type="text" class="form-control" id="item-${
+			checkItem_id + 1
+		}-cat-${id}" placeholder="Nombre del ítem"><br />
+				<input type="text" class="form-control" id="item-${
+					checkItem_id + 1
+				}-cat-${id}-description"  placeholder="Descripción del ítem" />
+				<button class="btn btn-primary"  id="item-${checkItem_id + 1}-cat-${id}-button">+</button><br />`;
+		document.getElementById(`category-demo-${id}`).innerHTML += `<br /><label for="item-${
+			checkItem_id + 1
+		}-cat-${id}-demo"></label><br />
+				<input class="form-check-input" id="item-${
+					checkItem_id + 1
+				}-cat-${id}-demo" type="checkbox" /><br />`;
 	};
 	const substractItem = () => {};
 
@@ -58,30 +77,39 @@
 	let nombreCheckCategory = '';
 
 	const editCatName = (element) => {
-		console.log('Mi elemento dentro de editCatName: ', element);
 		let catId = element.split('_').pop();
 		catId = catId.split('"');
 		let catNumber = catId[0].split('-').pop();
-		let catDemo = 'category-demo-'+catNumber;
+		let catDemo = 'category-demo-' + catNumber;
 
-		console.log('catId', catNumber);
-		console.log('Category blah blah: ', categoryCollection[catNumber-1]);
 		let categoryName: string = catNumber + ' - Trululu';
-		categoryCollection[catNumber-1].categoryName = categoryName;
+		categoryCollection[catNumber - 1].categoryName = categoryName;
 		document.getElementById(catId[0]).innerHTML = `<h5>${categoryName}</h5>`;
 		document.getElementById(catDemo).innerHTML = `<h5>${categoryName}</h5>`;
-
 	};
-	
-	const addCategory = () => {
 
-		// Renombrar categoría
+	onMount(() => {
+		// Handler de eventos
 		document.addEventListener('click', function (e) {
 			if (e.target) {
-				editCatName(JSON.stringify(e.path[0].id));
+				// alert(e.target.id);
+				// editCatName(JSON.stringify(e.path[0].id));
+
+				if (e.target.id.includes('addItem')) {
+					console.log('El target tiene un ', e.target.id);
+					addItem(e.target.id.split('-').pop());
+				}
+				if (e.target.id.includes('substractItem')) {
+					console.log('El target tiene un ', e.target.id);
+				}
+				if (e.target.id.includes('item') && e.target.id.includes('button')) {
+					console.log('El target tiene un ', e.target.id);
+				}
 			}
 		});
+	});
 
+	const addCategory = () => {
 		let newCategoryDivForm = document.createElement('div');
 		let newCategoryDivDemo = document.createElement('div');
 		let newItemDiv = document.createElement('div');
@@ -98,22 +126,23 @@
 		newCategoryDivForm.setAttribute('id', catDomId);
 		newCategoryDivForm.innerHTML = `<h5>${newCategory.category_id + 1} - ${
 			newCategory.categoryName
-		}</h5><Button id="btn_${catDomId}">Botón</Button><br />
-				<input type="text bind:value="${newCategory.categoryName}" placeholder="${
-			newCategory.category_id + 1
-		} - ${newCategory.categoryName}" />`;
+		}</h5>`;
+		// <button class="btn btn-primary"  id="btn_${catDomId}">Editar nombre categoría</button><br />
+		// 	<input class="form-control" hidden type="text bind:value="${newCategory.categoryName}" placeholder="${newCategory.category_id + 1} - ${newCategory.categoryName}" />`;
 		// newCategoryDivForm.innerHTML = `<h5>${newCategory.category_id+1} - ${newCategory.categoryName}</h5>`;
 
-		newItemDiv.setAttribute('id', 'item-container-' + categoryIndex);
-		newItemDiv.innerHTML = addItem();
+		newItemDiv.setAttribute('id', `item-container-${categoryIndex + 1}`);
+		//newItemDiv.innerHTML = addItem(categoryIndex+1);
 		newCategoryDivForm.appendChild(newItemDiv);
 
 		document.getElementById('itemsContainer').appendChild(newCategoryDivForm);
+		newCategoryDivForm.innerHTML += addButtons(categoryIndex + 1);
 
 		// Creo el elemento para la vista previa
-		newCategoryDivDemo.setAttribute('id', 'category-demo-' + categoryIndex);
+		newCategoryDivDemo.setAttribute('id', `category-demo-${categoryIndex + 1}`);
 		newCategoryDivDemo.innerHTML = `<h5>${categoryIndex + 1} - ${nombreCheckCategory}</h5>`;
 		document.getElementById('itemsContainerDemo').appendChild(newCategoryDivDemo);
+
 		// Parámetros finales
 		categoryIndex += 1;
 		nombreCheckCategory = '';
@@ -121,10 +150,15 @@
 	};
 
 	const substractCategory = () => {};
-	const addButtons = () => {};
-	const substractButtons = () => {};
+	const addButtons = (id) => {
+		return `<button class="btn btn-primary" id="addItem-${id}">+</button><button class="btn btn-primary" id="substractItem-${id}">-</button>`;
+	};
+	const substractButtons = (id) => {
+		return `<button class="btn btn-primary" id="substractCategory-${id}" on:click={substractCategory}>-</button>`;
+	};
 
 	/* ************************ Valores demo ************************ */
+
 	let nombreCheck = 'Control de Camionetas - Salida';
 	let optionsCheck = [
 		{
@@ -140,6 +174,8 @@
 	];
 	let selectedOption = 1;
 	/* ************************ Valores demo ************************ */
+
+
 </script>
 
 <svelte:head>
@@ -160,8 +196,12 @@
 	<div id="formCheck" class="col-6">
 		<h2>Datos de la Checklist</h2>
 		<label for="checklistName">Nombre</label><br />
-		<input type="text" placeholder="Control de Camionetas - Salida" bind:value={nombreCheck} /><br
-		/>
+		<input
+			class="form-control"
+			type="text"
+			placeholder="Control de Camionetas - Salida"
+			bind:value={nombreCheck}
+		/><br />
 
 		<!--<label for="checklistGroup">Grupo</label><br />
 		<select bind:value={selectedOption}>
@@ -170,14 +210,10 @@
 			{/each}
 		</select> -->
 
-		<Button id="addCategory" on:click={toggle}>
+		<button class="btn btn-primary" id="addCategory" on:click={toggle}>
 			<!-- <img src="/img/add.png" alt="agregar categoría" id="tinyAdd" /> -->
-			Agregar categoría</Button
+			Agregar categoría</button
 		>
-		<Button id="substractCategory" on:click={substractCategory}>
-			<!-- <img src="/img/sub.png" alt="remover categoría" id="tinySubs" /> -->
-			-
-		</Button>
 
 		<h4>Items</h4>
 
@@ -186,9 +222,15 @@
 			<!-- Acá se agregan las categorías -->
 		</div>
 		<label for="checklistItem-1">Item 1</label><br />
-		<input id="checklistItem-1" type="text" placeholder="Control Carga Extintor" /><br />
+		<input
+			class="form-control"
+			id="checklistItem-1"
+			type="text"
+			placeholder="Control Carga Extintor"
+		/><br />
 		<label for="checklistItemDescription-1">Descripción ítem 1</label><br />
 		<input
+			class="form-control"
 			id="checklistItemDescription-1"
 			type="text"
 			placeholder={optionsCheck[0].description}
@@ -196,22 +238,28 @@
 		/><br />
 
 		<label for="checklistItem-2">Item 2</label><br />
-		<input id="checklistItem-2" type="text" placeholder="Control Presión de Neumáticos" /><br />
+		<input
+			class="form-control"
+			id="checklistItem-2"
+			type="text"
+			placeholder="Control Presión de Neumáticos"
+		/><br />
 		<label for="checklistItemDescription-2">Descripción ítem 2</label><br />
 		<input
+			class="form-control"
 			id="checklistItemDescription-2"
 			type="text"
 			placeholder={optionsCheck[1].description}
 			bind:value={optionsCheck[1].description}
 		/><br />
-		<Button id="addItem">
-			<!-- <img src="/img/add.png" alt="agregar ítem" id="tinyAdd" /> -->
+		<!-- <button class="btn btn-primary"  id="addItem">
+			<img src="/img/add.png" alt="agregar ítem" id="tinyAdd" />
 			+
-		</Button>
-		<Button id="substractItem">
-			<!-- <img src="/img/sub.png" alt="remover ítem" id="tinySubs" /> -->
+		</button>
+		<button class="btn btn-primary"  id="substractItem">
+			<img src="/img/sub.png" alt="remover ítem" id="tinySubs" />
 			-
-		</Button>
+		</button> -->
 	</div>
 
 	<div id="vistaPrevia" class="col-6">
@@ -220,9 +268,9 @@
 			<h4>{nombreCheck}</h4>
 			<h5>{selectedOption} - {optionsCheck[selectedOption - 1].item}</h5>
 			<div id="itemsContainerDemo">
-				<input type="checkbox" />
+				<input class="form-check-input" type="checkbox" />
 				{optionsCheck[0].description} <br />
-				<input type="checkbox" />
+				<input class="form-check-input" type="checkbox" />
 				{optionsCheck[1].description} <br />
 			</div>
 		</form>
@@ -232,11 +280,16 @@
 		<Modal isOpen={open} {toggle}>
 			<ModalHeader {toggle}>Agregar categoría</ModalHeader>
 			<ModalBody>
-				<input type="text" bind:value={nombreCheckCategory} placeholder="ingresar un nombre" />
+				<input
+					class="form-control"
+					type="text"
+					bind:value={nombreCheckCategory}
+					placeholder="ingresar un nombre"
+				/>
 			</ModalBody>
 			<ModalFooter>
-				<Button color="primary" on:click={addCategory}>Agregar</Button>
-				<Button color="secondary" on:click={toggle}>Cancelar</Button>
+				<button class="btn btn-primary" color="primary" on:click={addCategory}>Agregar</button>
+				<button class="btn btn-primary" color="secondary" on:click={toggle}>Cancelar</button>
 			</ModalFooter>
 		</Modal>
 	</div>
