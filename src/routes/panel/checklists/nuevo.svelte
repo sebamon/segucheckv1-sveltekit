72fed2b4ebaa33f753f070274614ea4f9f913dde
xTorettox <fg.garciacendra@gmail.com>
2021-12-06 02:43:14 -0300

    ChecklistV2 con mejoras, documentado. Falta guardar y solucionar detalles

--- src/routes/panel/checklists/nuevo.svelte
+++ src/routes/panel/checklists/nuevo.svelte
@@ -1,7 +1,22 @@
+<script context="module">
+	export async function load() {
+		const response = await fetch('http://localhost:3000/api/category');
+		const data = await response.json();
+		//console.log(data)
+		return {
+			props: {
+				data
+			}
+		};
+	}
+</script>
+
 <script lang="ts">
 	// Importar por nombre de componentes: https://sveltestrap.js.org/
 	import { Button, Breadcrumb, BreadcrumbItem } from 'sveltestrap';
-	import Checklists from '$lib/Checklists.svelte';
+	import Checklists from '$lib/ChecklistsV2.svelte';
+
+	export let data;
 </script>
 
 <svelte:head>
@@ -24,6 +39,5 @@
 		<p class="lead">Ingrese los detalles a continuación.</p>
 	</div>
 </header>
-
-<Checklists />
+<Checklists {data}/>
 
