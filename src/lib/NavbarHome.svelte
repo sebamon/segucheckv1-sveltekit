c3d6069631aa5d6a4ee89e530e50c9423ccfca08
Arian <arian_eb@hotmail.com>
2021-12-14 15:08:30 -0300

    Se alterna botón login en inicio por Ir al panel

--- src/lib/NavbarHome.svelte
+++ src/lib/NavbarHome.svelte
@@ -54,8 +54,8 @@
 				<NavLink href="#contacto" class="text-primary">Contacto</NavLink>
 			</NavItem>
 			<NavItem class="ms-2">
-				<Button color="primary" on:click={() => (modalOpen = !modalOpen)}>Iniciar sesión</Button>
-				<Button color="warning" href="/panel" title="placeholder">Ir al panel</Button>
+				<!-- <Button color="primary" on:click={() => (modalOpen = !modalOpen)}>Iniciar sesión</Button> -->
+				<Button color="warning" href="/panel">Ir al panel</Button>
 			</NavItem>
 		</Nav>
 	</Collapse>
